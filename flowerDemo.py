348af069d25685ae084b0aa566f3f352ee2889b5
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-04-26 08:31:54 -0400

    Updated from Brython Server: 4/26/2018 8:31:53 AM

--- flowerDemo.py
+++ flowerDemo.py
@@ -1,1 +1,9 @@
+#Ryan Jones
+#4/26/18
+#flowerDemo.py - vowelWordDemo.py I misheard the name - treat strings as lists
 
+words = input('Enter some words: ').split(' ')
+
+for w in words:
+    if w[0] in 'AEIOUaeiou': #starts with a vowel
+        print(w)
