73d3f6984c71d06d0bec39ce0f9dadc1acc2d6ac
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-05-02 08:40:28 -0400

    Updated from Brython Server: 5/2/2018 8:40:27 AM

--- cocktailSortDemo.py
+++ cocktailSortDemo.py
@@ -17,7 +17,8 @@ def mySort(A):
                 swapped = True
         if not swapped:
             break
-        for i in range(len(A)-2,-1):
+        swapped = False
+        for i in range(len(A)-2,-1,-1):
             if A[i] > A[i+1]:
                 A[i], A[i+1] = A[i+1], A[i] #swap in python
                 swapped = True
