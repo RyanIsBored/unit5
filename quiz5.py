792adf728d66c63a6a483ab200c4cad2a2670e75
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-05-08 08:29:22 -0400

    Updated from Brython Server: 5/8/2018 8:29:22 AM

--- quiz5.py
+++ quiz5.py
@@ -1,1 +1,15 @@
+#Ryan Jones
+#5/8/18
+#quiz5.py
+
+def penultimate(list):
+    list = ['a','b','c','d']
+    print(list[-2])
+
+penultimate(list)
+
+e=int(input('Enter a list of numbers: '))
+
+
+    
 
